be133610abbd2ec94161ecdfa8947b815dd32015
Bongani-Cyprian <mkhizebongani16@gmail.com>
2025-07-25 15:41:49 +0200

    chore: update .gitignore to exclude bytecode, add CI workflow and tests

---
+++ tests/test_dag.py
@@ -0,0 +1,8 @@
+﻿from airflow.models import DagBag
+
+def test_dag_imports():
+    dagbag = DagBag(include_examples=False)
+    dag = dagbag.get_dag('e_commerce_etl')
+    assert dag is not None
+    # expect three tasks: load_sales, load_products, transform
+    assert len(dag.tasks) == 3
